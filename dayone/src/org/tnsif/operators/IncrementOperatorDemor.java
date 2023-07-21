28c7e2865cf8105bf4874e2d5ba3e705cfb53a07
Roma-Thakur <romathakur775@gmail.com>
2023-07-21 11:31:19 +0530

    second commit

---
+++ dayone/src/org/tnsif/operators/IncrementOperatorDemor.java
@@ -0,0 +1,16 @@
+//Program to demonstrate on Pre and Post Increment Operators
+package org.tnsif.operators;
+
+public class IncrementOperatorDemor {
+
+	public static void main(String[] args) {
+		int p=11,q=2;
+		int x=p++;
+		int y=p+q;
+		++q;
+		System.out.println(p);
+		System.out.println(q);
+
+	}
+
+}
