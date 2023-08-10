9b59b24978332a85c30625fedb43c40ba896ed8a
Roma-Thakur <romathakur775@gmail.com>
2023-08-10 11:37:44 +0530

    second commit

---
+++ dayten/src/org/tnsif/wrapperclassdemo/AutoBoxing.java
@@ -0,0 +1,13 @@
+package org.tnsif.wrapperclassdemo;
+/*Program to demonstrate on AutoBoxing
+ * AutoBoxing: Conversion of Primitive type to Object type*/
+public class AutoBoxing {
+
+	public static void main(String[] args) {
+		char c='A';
+		//converting a primitive data type into object type
+		Character ch=c;
+		System.out.println(ch);
+	}
+
+}
---
+++ dayten/src/org/tnsif/wrapperclassdemo/UnBoxing.java
@@ -0,0 +1,13 @@
+package org.tnsif.wrapperclassdemo;
+/*Program to demonstrate on UnBoxing
+ * UnBoxing: Conversion of Object type to Primitive type*/
+public class UnBoxing {
+
+	public static void main(String[] args) {
+		Integer num=34;
+		//converting object to primitive type
+		int x=num;
+		System.out.println(x);
+	}
+
+}
